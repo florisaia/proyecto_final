73ade8ef0d9cd5eb3bf8af00a02297f7799f5550
florisaia <115759653+florisaia@users.noreply.github.com>
2022-12-20 18:43:04 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+
+function nocturno() {
+    document.body.style.backgroundImage="none";
+    document.body.style.backgroundColor="black";
+    document.header.style.backgroundImage="none";
+    document.getElementById("abanicos").style.backgroundColor="grey";
+}
+
+function oscuro() {
+    document.body.style.backgroundColor="black";
+    document.body.style.backgroundImage="none";
+    document.getElementById("card1").style.backgroundColor="darkgrey";
+    document.getElementById("card2").style.backgroundColor="darkgrey";
+}
+
+function copyright() {alert ("Desarrollador: Florencia Isaía - Rafaela, Santa Fe -                            Email: flor.isaia10@gmail.com                                              GitHub:florisaia")} 
